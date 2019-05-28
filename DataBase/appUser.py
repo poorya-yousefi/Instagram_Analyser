6e84a458cc5292df37002872fe2a585e3bcfcd82
poorya-yousefi <poorya3976@gmail.com>
2019-05-28 20:22:55 +0430

    update appUser class and subclass

--- DataBase/appUser.py
+++ DataBase/appUser.py
@@ -2,18 +2,28 @@ import datetime
 
 
 class AppUser:
-    def __init__(self, lastLoginDate: datetime, hasTwoStep: bool,
-                 country: str, city: str, phoneNum: str,
-                 personalityKeyWords: str, cookie_path: str):
+    def __init__(self, last_login_date: datetime, has_two_step: bool,
+                 country: str, city: str, phone_num: str,
+                 personality_key_words: str, cookie_path: str):
         self.userId = -1
         self.signupDate = None
-        self.lastLoginDate = lastLoginDate
-        self.hasTwoStep = hasTwoStep
+        self.lastLoginDate = last_login_date
+        self.hasTwoStep = has_two_step
         self.country = country
         self.city = city
-        self.phoneNum = phoneNum
-        self.personalityKeyWords = personalityKeyWords
+        self.phoneNum = phone_num
+        self.personalityKeyWords = personality_key_words
         self.cookie_path = cookie_path
 
-    def __str__(self):
+    def __str__(self):  # TODO
         return "<Id: {0} | lastLogin: {1}>".format(self.userId, self.lastLoginDate)
+
+
+class CommercialUser(AppUser):
+    def __init__(self, last_login_date: datetime, has_two_step: bool,
+                 country: str, city: str, phone_num: str,
+                 personality_key_words: str, cookie_path: str, company_name: str, activity: str):
+        AppUser.__init__(self, last_login_date, has_two_step, country, city, phone_num, personality_key_words,
+                         cookie_path)
+        self.companyName = company_name
+        self.activity = activity
